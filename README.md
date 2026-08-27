2df33dd93981d700f97dafb0d765099e482ca60a
sarani6383575635-cyber <Sarani6383575635@gmail.com>
2026-08-27 19:52:38 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Student mark analyzer
+A simple python project that calculate student marks, average,grade, and pass/fail status.
+## Features
+- Accept students name
+- Accept marks for five subjects
+- calculates total marks
+- calculates average marks
+- assigns a grade 
+- determines pass/fail status
+- saves student results to a csv file
+## subjects
+this project currently uses:
+- python 
+- database
+-  maths
+- java
+- DS
+## technologies used
+- python
+- csv
+- git
+- github
+## how to run
+open the project in vs code.
+open the terminal and run:
+ ```bash
+ python main.py
+
---
+++ main.py
@@ -0,0 +1,51 @@
+import csv
+import os
+
+name = input("enter student name:")
+Subjects = ["python","database","maths","java","DS"]
+marks = []
+for Subject in Subjects:
+    mark = float (input(f"enter marks for {Subjects}:"))
+    marks.append(mark)
+total = sum(marks)
+average = total / len(marks)
+if average >= 90:
+    grade = "A+"
+elif average >= 80:
+    grade = "A"
+elif average >= 70:
+    grade = "B"
+elif average >= 60:
+    grade = "B+"
+elif average >= 50:
+    grade = "D"
+else:
+    grade = "F"
+result ="pass" if average >= 40 else "fail"
+print(f"Student name: {name}")
+print(f"Total marks: {total}")
+print(f"average: {average:.2f}")
+print(f"grade: {grade}")
+print(f"result: {result}")
+
+file_name = "Student_results.csv"
+file_exists = os.path.exists(file_name)
+with open(file_name,"a",newline = "") as file:
+    writer = csv.writer(file)
+    if not file_exists:
+        writer.writerow([
+            "Student name",
+            "Total marks",
+            "average",
+            "grade",
+            "results"
+        ])
+
+    writer.writerow([
+        name,
+        total,
+        round(average,2),
+        grade,
+        result
+    ])    
+print("\nResult has been saved to Student_results.csv")
